4b4652f57897d189f506bf673dd301d0b3d588ec
LALU HILMAN ALFARISYI - 1206208252 <lalu.hilman@bulan.cs.ui.ac.id>
2015-09-16 11:48:02 +0700

    functional test peacok 2

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,17 @@
+#from django.shortcuts import render
+
+#def home_page(request):
+ #   return render(request, 'home.html')
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+#def home_page(request):
+ #   if request.method == 'POST':
+  #      return HttpResponse(request.POST['item_text'])
+   # return render(request, 'home.html')
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text',''),
+    })
+
